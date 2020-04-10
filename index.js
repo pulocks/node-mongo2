d06845f144f3b3bf1ee5a54065c8a6673e52c52b
pulocks <pulock_kuet@yahoo.com>
2020-04-10 21:14:46 +0600

    mongodb integration with react

--- index.js
+++ index.js
@@ -35,6 +35,51 @@ app.get('/products', (req, res) => {
       });  
 })
 
+app.get('/product/:key', (req, res) => {
+
+    const key = req.params.key;
+
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        // perform actions on the collection object
+        collection.find({key}).toArray((err, documents) => {
+            if(err) {
+                console.log(err);
+            }
+            else{
+                res.send(documents);
+            }
+            
+        });
+        //client.close();
+      });  
+})
+
+app.post('/getProductsByKey', (req, res) => {
+
+    const key = req.params.key;
+    const productKeys = req.body;
+    console.log(productKeys);
+
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        // perform actions on the collection object
+        collection.find({key: { $in: productKeys }}).toArray((err, documents) => {
+            if(err) {
+                console.log(err);
+            }
+            else{
+                res.send(documents);
+            }
+            
+        });
+        //client.close();
+      });  
+})
+
+
 // POST
 app.post('/addProduct', (req, res) => {
     const product = req.body;
@@ -42,12 +87,31 @@ app.post('/addProduct', (req, res) => {
     client.connect(err => {
         const collection = client.db("onlineStore").collection("products");
         // perform actions on the collection object
-        collection.insertOne(product, (err, result) => {
+        collection.insert(product, (err, result) => {
             if(err) {
                 console.log(err);
             }
             else{
-                console.log('Successfully Inserted', result.ops[0]);
+                res.send(result.ops[0]);
+            }
+            
+        });
+        //client.close();
+      });  
+});
+
+app.post('/placeOrder', (req, res) => {
+    const orderDetails = req.body;
+    orderDetails.orderTime = new Date();
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("orders");
+        // perform actions on the collection object
+        collection.insertOne(orderDetails, (err, result) => {
+            if(err) {
+                console.log(err);
+            }
+            else{
                 res.send(result.ops[0]);
             }
             
